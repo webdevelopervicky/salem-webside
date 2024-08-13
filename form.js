618ac2988a92a7a5f6b3c6d2fc770fefbf12c829
webdevelopervicky <vickydeveloper0@gmail.com>
2024-08-13 22:37:43 +0000

    upload

---
+++ form.js
@@ -0,0 +1,546 @@
+// //  var a=5;
+// //  var fname="name";
+// //  var bname=false;
+// //  const s1=Symbol();
+// //  const s2=Symbol()
+
+// //  console.log(a);
+// //  console.log(typeof a);
+
+// //  console.log(fname);
+// //  console.log(typeof fname)
+
+// //  console.log(typeof bname);
+// //  console.log(s1)
+// //  console.log(s2)
+// //  console.log(s1!=s2);
+
+// // const ar=['vicky','dhineh','tamil,suresh']
+// // console.log(ar)
+// // console.log(typeof ar);
+
+
+// // const st={
+// //     'name':'vicky',
+// //     'age':'30',
+// // }
+// // console .table(st)
+
+// // var d=new Date();   
+// // console.log(d);
+
+// // var n=0;
+// // console.log(n,typeof a)
+
+// // var n=Boolean(0);
+// // console.log(n,typeof n)
+
+// // let f=30;
+// // let l=40;
+// // let c=f+l;
+// // c=2**5
+// // // c=++fc
+// // c=--f
+
+// // console.log(c);
+
+// // let a=12;
+// // let b=30;
+
+// // console.log("greater:",a>b);
+// // console.log("lesser:",a<b);
+// // console.log("lesser:",a<=b);
+
+
+// // logical operators
+
+// // let mark=45
+
+// // console.log(mark>35 && mark<100);
+
+// // console.log(mark==2||mark==45);
+
+// // var age=15;
+// // var result=age>=18?"eligable":"not eligable";
+// // console.log(result);
+// // var mark=25
+// // var result=mark>=35?"pass":"fail";
+// // console.log(result);
+
+// // function welcome(name){
+// //     const result=name?name:"not name"
+// //     console.log(result)
+
+// // }
+// // welcome("vicky");
+
+// // var mark1=99;
+// // var mark2=80;
+// // var mark=90;
+
+// // var grade=mark1>90?"A grade":grade=mark1>70?"B grade":"no grade";
+// // console.log(grade);
+
+// // var a=24;
+// // var b=50;
+// // console.log(a&b);
+
+
+// // var a=5;
+// // var b=12;
+// // console.log(~a+~b)
+
+// // // a<<=b;
+// // // console.log(a)
+
+// // a=8;
+// // b=1;
+// // console.log(a>>b);
+// // a=null;
+
+// // var a=null??"no value";
+// // console.log(a);
+
+
+// // var user={'name':'vicky'};
+
+// // user.city??='salem';
+// // console.log(user.city);
+// // console.log(user);
+
+
+// // let age=prompt("enter your age : ");
+// // if(age>18){
+// //     console.log("your are eligable")
+// // }
+// // else{
+// //     console.log("you are not yeligable")
+// // }
+
+
+// // var mark=prompt("enter tha mark");
+
+// // if(mark>=90 && mark<=100){
+// //     console.log("A Grade");
+// // }
+// // else if(mark>=80 && mark<=89){
+
+// //     console.log("B Grade");
+// // }
+// // else if(mark>=70&&mark<=79){
+// //     console.log("C Grade")
+// // }
+// // else {
+// //     console.log("D Grade")
+// // }
+
+// // var tamil=20,english=45,maths=50;
+// // total=tamil+english+maths;
+// // avg=total/3;
+// // console.log(avg.toFixed())
+// // if(tamil>=35 && english>=35 && maths>=35)
+// //     {
+// //     console.log("you are pass");
+// //      if(avg>=90 && avg>=100){
+// //         console.log("A Grade");
+// //     }
+// //     else if(avg>=80 && avg<=89);
+// //     {
+// //     console.log("B Grade");
+// //     }
+
+// // }
+// // else{
+// //     console.log("you are fail")
+// // }
+// // // switch
+
+// // var num=8;
+// // switch(num){
+// //     case 1:
+// //         console.log("one")
+// //         break
+// //     case 2:
+// //         console.log("two")
+// //         break;
+    
+// //     case 3:
+// //         console.log("three")
+// //         break;
+// //     default:
+// //         console.log("no value")
+// // }
+// // var letter='c';
+// // switch(letter){
+// //     case'a':
+// //     case'e':
+// // //     case'i':
+// // //     console.log(letter+" :it is a vowel");
+// // //     default:
+// // //         console.log(letter+" :it is a non vowel")
+// // // }
+
+// // // let a=1;
+// // // while(a<=10){
+// // //     console.log(a);
+// // //     a++;
+// // // }
+
+// // // let tabel=5;
+// // // let limit=10;
+// // // let i=;
+
+// // // do{
+// // //     console.log(i+"x"+tabel+"="+(i*tabel));
+// // //     i++;
+// // // }
+// // // while(i<=10)
+
+// // // for(let a=2;a<=10;a+=2){
+// // //     console.log(a);
+// // // // }
+// // // let arr=[];
+// // // for  (let a=0;a<=100;a+=2){
+// // //     arr.push(a)
+    
+// // // }
+// // // console.log(arr[2]);
+
+// // //  let num=[];
+// // //  for (let i=0;i<3;i++){
+// // //     num.push([])
+// // //    for(let v=0;v<3;v++){
+// // //     num[i].push(v);
+// // //    }   
+// // //  }   
+// // //  console.table(num);
+
+// // // var nums=[];
+// // // for(let i=0;i<=3;i++){
+// // //   nums.push([]);
+// // //   console.log(nums)
+// // //   for (let j=0;j<=3;j++){
+// // //     nums[i].push(j);
+// // //   }
+// // // }
+// // // console.table(nums)
+
+// // // let names=["vicky","dhinesh","suresh","tamil"]
+
+// // // for(let i=0;i<names.length;i++){
+// // //    console.log(names[i]);  
+// // // }
+
+// // // for(let vicky of names){
+// // //   console.log(vicky)
+// // // }
+
+// // // let user={
+// // //  name:"vicky",
+// // //  age:"20",
+// // //  city:"salem",
+// // //  contact:8940680540
+// // // }
+// // // for(let prop in user)
+// // // {
+// // //   console.log(prop+" :"+user[prop])
+// // // }
+
+// // // let user={
+// // //     name:"vicky",
+// // //     age:"20",
+// // //     city:"salem",
+// // //     contact:"8940680540"
+// // //    }
+// // // let vicky=Object.keys(user);
+// // // let vickyvalue=Object.values(user);
+// // //  for(let i=0;i<vickyvalue.length;i++)
+
+// // // console.log(vicky[i]+" :"+vickyvalue[i])
+
+// // // let vicky=Object.keys(user);
+// // // let vvalue=Object.values(user);
+// // // for(let i=0;i<vicky.length;i++)
+// // // {
+// // //   // console.log(vicky[i]+" :"+vv alue[i]);
+// // //   console.log(user[vicky[i]]);
+// // // }
+
+// // // for(let i=1;i<=10;i++){
+ 
+// // //   if(i%2==0){
+// // //    continue;
+// // //   }
+// // //   console.log(i);
+// // // }
+
+// // // let groups = 
+// // // [
+// // //   ["sicky","dhinesh","suresh"],
+// // //   ["tamil","rangith","surya"],
+// // //   ["ngglish","picky","kugan"],
+// // // ];
+
+// // // for(let group of groups)
+// // // { 
+// // //   inner:
+// // //   for(let memb of group){
+// // //    if(memb.startsWith("s")){
+// // //     console.log("first letter :"+memb);
+// // //     break inner;
+// // //    }
+// // //   }
+
+
+// // // function sum(...vicky){
+// // //   total=0
+// // //   for(let i=0;i<vicky.length;i++)
+// // //   {
+// // //     total+=vicky[i];
+// // //   }
+// // //   return total;
+// // // }
+// // // console.log(sum(25,45,70,60,60));
+
+// // // function total(...vicky){
+// // //   sum=0
+// // //  for(let i=0;i<vicky.length;i++)
+// // //   {
+// // //   sum+=vicky[i]
+// // //  }
+// // //  return sum;
+
+// // // }
+// // // console.log(total(35,54))
+
+// // // let vicky=function(a,b){
+// // //  return a+b;
+// // // }
+// // // console.log(vicky(30,30))
+
+// // const add=(a,b)=>{return a+b;};
+// // console.log(add(80,60));
+
+// // var numbers=[1,2,3,4,5,6];
+// // let double=numbers.map(number=>number*2)
+// // console.log(double);
+
+// // const fruids=["apple","banana","orange","grapegyi"]
+// // let fillter=fruids.filter(fruid=>fruid.length>5)
+// // console.log(fillter);
+
+
+// // let counter=vickycount=()=>{
+// //     let count=1;    
+// //     return ()=>{
+// //         count++;
+// //         return count;
+// //     }
+// // }
+// // console.log(counter());
+// // console.log(counter());
+
+// // setTimeout(function(){
+// //     console.log("hello world")
+// // },    
+// // 3000);
+
+// //  let numbers=[1,2,3,4,5,];
+// // let total=0;
+// // numbers.forEach(number=>{
+// //     total+=number;
+// // })
+
+// // // console.log(total)
+// // let doublenumber=[];
+// // numbers.forEach(number=>{
+// //     doublenumber.push(number*2);
+// // })
+// // console.log(doublenumber)
+// // // let max=numbers[0]
+// // // numbers.forEach(number=>{
+// // // if(number>max){
+// // //     max=number;
+// // // }
+// // // }
+// // // )
+// // // // console.log(max)
+
+// // // console.log(numbers)
+// // // total=0
+// // // numbers.forEach(number=>{
+// // //     total+=number;
+    
+// // // })
+// // // console.log(total/numbers.length)
+// // let names=[
+// //     "vicky","mikey","jhon","bob"
+// // ]
+
+// // names.forEach((name,intex)=>{
+// //   names[intex]=name.toUpperCase()
+// // })
+// // console.log(names)
+// fruits=["apple","banana","graaps"]
+// colors=["red","yellow","blue"]
+
+// var fruitscolors=fruits.map((fruit,intex)=>fruit+" "+colors[intex])
+// console.log(fruitscolors);
+
+// var Numbers=[1,2,3,4,5,6,7,8]
+
+// var numbervariable=Numbers.map(number=>number%2===0);
+// console.log(numbervariable);
+
+// const users=[
+//     {name:"vicky",age:20},
+//     {name:"dhinesh",age:"20"},
+//     {name:"tamil",age:"19"}
+// ]
+// const uservariaples=users.map(user=>user.age)
+// console.log(uservariaples);
+
+// const uptate=users.map(user=>{
+//     return{
+//         name:user.name,
+//         age:user.age+1
+//     }
+// })
+// console.log(uptate)
+// //------------
+
+// // let words=["apple","banana","cherry"];
+// // const worldselect=words.map(function(value,intex,array){
+// //     return{
+// //         word:value,
+// //         length:value.length,
+// // //         position:intex,
+// // //         total:array,
+// // //     };
+// // // }
+// // // )
+// // // console.log(worldselect)
+// // let items=[
+// //     {name:"apple",catagory:"fruid"},
+// //     {name:"banana",catagory:"fruid"},
+// //     {name:"tomata",catagory:"vegetble"}
+// // ];
+// // let label=items.filter(item=>item.catagory==="fruid")
+// // console.log(label)
+
+// let words=["cat","elephand","dog","fish"]
+// let startc=words.filter(word=>word.startsWith("e"));
+// console.log(startc)
+
+// let carts=[
+//     {name:"orange",catoagory:"fruit",price:100},
+//     {name:"apple",catoagory:"fruit",price:120},
+//     {name:"tomato",catoagory:"vegetable",price:110}
+// ]
+// let cartfilter=carts.filter(cart=>cart.catoagory==="fruit"&& cart.price>=100);
+// console.log(cartfilter)
+
+// let numbers=[[1,2],[3,4],[5,6],[7,8],[9,10]]
+// let mixarray=numbers.reduce((anyvalue,currentvalue)=>anyvalue.concat(currentvalue))
+
+// document.getElementById("demo").innerHTML=mixarray;
+
+function myfunction(p,x){
+    return p*x;
+}
+ let results=myfunction(5,8);
+document.getElementById("demo1").innerHTML="the value is = "+results;
+// console.log(result);
+
+function vicky(a,b){
+    return a*b;
+}
+let result=vicky(30,40);
+document.getElementById("demo2").innerHTML="the value is = "+result;
+
+function typeofcars(){
+    let text="cars"
+    let types=text +" "+ typeof text;
+    document.getElementById("demo3").innerHTML=types;
+console.log(types)
+}
+typeofcars()
+
+// var objectsthinks={
+//     type:"BMW",modal:"500",color:"red"
+// };
+// document.getElementById("demo").innerHTML="type oxf car "+car.type;
+
+let number=[1,2,3,4,5,6,7,8,9];
+let removed=number.slice(2);
+document.getElementById("demo4").innerHTML=removed;
+
+let a=[1,2,3,4,5,6,7,8]
+a.splice(2,0,100,200,);
+document.getElementById("demo5").innerHTML=a;
+
+const b=[10,22,30,40];
+const c=[55,66,77,88];
+const d=["vicky","suresh"];
+
+e=a.concat(b,d);
+document.getElementById("demo6").innerHTML=e;
+
+let num=[1,2,8,4,6,5,7,3];
+num.sort();
+document.getElementById("demo7").innerHTML=num;
+
+let numbe=[100,250,738,54,660,85,97,23];
+numbe.sort((a,b)=>
+{return a-b}
+);
+document.getElementById("demo8").innerHTML=numbe;
+
+let n=[1,2,34,5,6,7,8]
+n.fill(35,3,5);
+document.getElementById("demo8").innerHTML=n;
+
+let think=["pen","papper","pencil","notes"]
+let product=think.includes("pen");
+document.getElementById("demo9").innerHTML=product;
+
+let joinproduct=["pen","papper","pencil","notes"]
+
+document.getElementById("demo10").innerHTML=joinproduct.join(" ");
+
+let x=[1,2,3,4,5,6,7]
+x.reverse();
+document.getElementById("demo11").innerHTML=x;
+
+let user1=["suresh","vicky","dhinesh"];
+let user2=["tamil","suresh","ranjith"];
+user1.push(...user2);
+document.getElementById("demo12").innerHTML=user1.join(" ");
+
+let user3=["suresh","vicky","dhinesh"];
+let element=user3.shift();
+
+let y=[12,14,16,18];
+ resulty=y.every((value)=>{
+    return value%2==0
+ }
+)
+document.getElementById("demo13").innerHTML=resulty;
+console.log(resulty)
+
+// -----*/
+
+function student(fullname,fathername,age,address,city){
+const user={};
+user.fullname=fullname;
+user.fathername=fathername;
+user.age=age;
+user.address=address;
+user.city=city;
+user.about=function(){
+    return '${this.fullname} is from ${this.city}';
+}
+}
+
+
+ 
